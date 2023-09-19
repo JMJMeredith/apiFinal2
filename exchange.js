3317a9d6ee87089b53b3166e990a622ab90d1db1
JMJMeredith <123377012+JMJMeredith@users.noreply.github.com>
2023-09-19 20:48:59 +0000

    update10

--- exchange.js
+++ exchange.js
@@ -7,23 +7,28 @@ const API_KEY = " YQkgdt+GRxShdkARaBBr7A==98r4hlrCq7qXKBfY "
 const apiUrl = "https://api.api-ninjas.com/v1/exchangerate?pair=USD_"
 
 convert.addEventListener('click', () =>{
-    const amountTotal = amount.ariaValueMax;
-    const currencyTotal = currency.ariaValueMax;
+    console.log('Hi')
+    const amountTotal = amount.value;
+    console.log('total',amountTotal)
+    const currencyTotal = currency.value;
     const url = apiUrl + currencyTotal;
+    console.log('url',url)
 
-    fetchurl, {
+    fetch, (url,{
         headers: {
             'X-API-KEY': apiKey
         }
-    }
+    })
     .then(response => response.json())
     .then(data => {
-        const rate = data.rate;
+        const rate = data.exchange_rate;
+        console.log('rate', rate)
         const resultPrice = amountTotal * rate;
+        console.log = ('result', resultPrice)
         result.innerHTML = '${amount} ${currency} = ${resultPrice.toFixed(2)} USD';
     })
     .catch(error => {
         console.error('request failed', error);
         result.innerHTML = ' Error. Please try again.'
     })
-    })
+})
